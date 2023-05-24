76862729b4a7ade29a3bbbceb520651dda95e3e6
Dusky7 <victorsantos12dima@gmail.com>
2023-05-24 17:26:53 -0300

    first commit: Preparando o ambiente

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Container } from './page.styles'
+
+export default function Home() {
+  return (
+    <Container>
+        <h1>Meu time</h1>
+        
+        <ul><li></li></ul>
+    </Container>
+  )
+}
---
+++ src/app/page.styles.js
@@ -0,0 +1,9 @@
+'use client'
+
+import styled from "styled-components";
+
+export const Container = styled.main`
+  background-color: #252f3a;
+  width: 100vw;
+  height: 100vh;
+`

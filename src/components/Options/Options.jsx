5747baf5643ef220ea967b87dfad48e5f00e1828
Victor Santos <victorsantos12dima@gmail.com>
2023-10-12 21:55:58 -0300

    Feat. Options box and modal functionality

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,37 +1,60 @@
 import { useState } from 'react';
 
-import { Content, TitleOptions, ArrowDown } from './styles';
+import { Content, BoxOptions, ArrowDown, Title } from './styles';
 
 export default function Options() {
-  const [modal, setModal] = useState(false);
-
-  function OpenModal() {
-    setModal(true);
-  }
+  const [modalPlayers, setModalPlayers] = useState(false);
+  const [modalFormation, setModalFormation] = useState(false);
+  const [modalTable, setModalTable] = useState(false);
+  const [modalNumberOfGoals, setModalNumberOfGoal] = useState(false);
 
   return (
     <>
-      <TitleOptions>
-        <h3>
-          Jogadores <ArrowDown />
-        </h3>
-        <Content>Jogador1</Content>
-      </TitleOptions>
+      <BoxOptions>
+        <Title onClick={() => setModalPlayers(true)}>Jogadores</Title>
+        <ArrowDown
+          onClick={() => setModalPlayers(false)}
+          isOpen={modalPlayers}
+        />
+      </BoxOptions>
 
-      <TitleOptions>
-        Formação mais usada na temporada <ArrowDown />
-      </TitleOptions>
-      <Content>Formação1</Content>
+      {modalPlayers && (
+        <Content setModalPlayers={setModalPlayers}>Jogador1 </Content>
+      )}
 
-      <TitleOptions>
-        Tabela de Resultados <ArrowDown />
-      </TitleOptions>
-      <Content>Tabela1</Content>
+      <BoxOptions>
+        <Title onClick={() => setModalFormation(true)}>
+          Formação mais usada na temporada
+        </Title>
+        <ArrowDown
+          onClick={() => setModalFormation(false)}
+          isOpen={modalFormation}
+        />
+      </BoxOptions>
 
-      <TitleOptions>
-        Quantidade de gols marcados por tempo de jogo <ArrowDown />
-      </TitleOptions>
-      <Content>Gols1</Content>
+      {modalFormation && (
+        <Content setModalFormation={setModalFormation}>Formação1</Content>
+      )}
+
+      <BoxOptions>
+        <Title onClick={() => setModalTable(true)}>Tabela de Resultados</Title>
+        <ArrowDown onClick={() => setModalTable(false)} isOpen={modalTable} />
+      </BoxOptions>
+
+      {modalTable && <Content setModalTable={setModalTable}>Tabela1</Content>}
+      <BoxOptions>
+        <Title onClick={() => setModalNumberOfGoal(true)}>
+          Quantidade de gols marcados por tempo de jogo
+        </Title>
+        <ArrowDown
+          onClick={() => setModalNumberOfGoal(false)}
+          isOpen={modalNumberOfGoals}
+        />
+      </BoxOptions>
+
+      {modalNumberOfGoals && (
+        <Content setModalNumberOfGoal={setModalNumberOfGoal}>Gols1</Content>
+      )}
     </>
   );
 }
--- src/components/Options/styles.js
+++ src/components/Options/styles.js
@@ -2,7 +2,7 @@ import { MdKeyboardArrowDown } from 'react-icons/md';
 
 import styled from 'styled-components';
 
-export const TitleOptions = styled.div`
+export const BoxOptions = styled.div`
   background: #7b7b7b;
   display: flex;
   justify-content: space-between;
@@ -14,17 +14,28 @@ export const TitleOptions = styled.div`
   margin-bottom: 22px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   cursor: pointer;
-  padding-left: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
   font-size: 13px;
+`;
+
+export const Title = styled.h3`
+  width: 100%;
+  height: 36px;
+  display: flex;
+  align-items: center;
+`;
+
+export const ArrowDown = styled(MdKeyboardArrowDown)`
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+  transform: ${(props) => (props.isOpen ? 'rotate(0)' : 'rotate(90deg)')};
+  animation: transform 0.8s ease-in;
 `;
 
 export const Content = styled.div`
   font-size: 12px;
   font-weight: 400;
   margin-bottom: 20px;
-`;
-export const ArrowDown = styled(MdKeyboardArrowDown)`
-  width: 19px;
-  height: 19px;
-  cursor: pointer;
 `;
